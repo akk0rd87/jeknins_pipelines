aac04a290f2a0a919c8f770bdb1cf9e21e4b02ef
Amir <amkad87@gmail.com>
2023-06-06 15:02:29 +0400

    +

--- vars/SDLCall.groovy
+++ vars/SDLCall.groovy
@@ -24,13 +24,13 @@ def call(String ProjectName, String DeployKey) {
         stages {
             stage('Checkout akkordsdk') {
                 steps {
-                    checkout scmGit(branches: [[name: "${AKK0RD_SDK_BRANCH}"]], extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'akkordsdk']], userRemoteConfigs: [[credentialsId: 'GithubDeployKey', url: 'https://github.com/akk0rd87/akk0rdsdk.git']])
+                    checkout scmGit(branches: [[name: "${AKK0RD_SDK_BRANCH}"]], extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'akkordsdk']], userRemoteConfigs: [[url: 'https://github.com/akk0rd87/akk0rdsdk.git']])
                 }
             }
 
             stage("Checkout project") {
                 steps {
-                    checkout scmGit(branches: [[name: "${PROJECT_BRANCH}"]], extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: '${AKK0RD87_GITHUB_PROJECT_NAME}']], userRemoteConfigs: [[credentialsId: '${AKK0RD87_DEPLOY_KEY}', url: 'git@github.com:akk0rd87/${AKK0RD87_GITHUB_PROJECT_NAME}.git']])
+                    checkout scmGit(branches: [[name: "${PROJECT_BRANCH}"]], extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${ProjectName}"]], userRemoteConfigs: [[credentialsId: "${DeployKey}", url: "git@github.com:akk0rd87/${ProjectName}.git"]])
                     sh 'chmod +x ${AKK0RD87_GITHUB_PROJECT_NAME}/proj.android/gradlew'
                 }
             }
