a742094182e8730bbaea17bef59923b7235c7ecd
Amir <amkad87@gmail.com>
2023-06-06 15:06:58 +0400

    +

--- vars/SDLCall.groovy
+++ vars/SDLCall.groovy
@@ -2,7 +2,7 @@ def gradleCall(GRADLE_TASK) {
     withCredentials([file(credentialsId: '${ANDROID_KEYSTOREKEY_FILE}'  , variable: 'ANDROID_KEYSTORE_KEY'),
                     file(credentialsId: '${ANDROID_KEYSTOREPARAM_FILE}', variable: 'ANDROID_KEYSTORE_PARAMS'),
                     file(credentialsId: 'GooglePlayApiCredentials'     , variable: 'ANDROID_GOOGLEPLAY_CREDS')]) {
-        sh '${WORKSPACE}/${ProjectName}/proj.android/gradlew -p ${WORKSPACE}/${ProjectName}/proj.android ' + GRADLE_TASK
+        sh "${WORKSPACE}/${ProjectName}/proj.android/gradlew -p ${WORKSPACE}/${ProjectName}/proj.android " + GRADLE_TASK
     }
 }
 
@@ -14,9 +14,6 @@ def call(String ProjectName, String DeployKey) {
         environment {
             ANDROID_SDK_ROOT='/opt/android-sdk'
             AKKORD_SDK_HOME="${WORKSPACE}/akkordsdk/"
-            AKK0RD87_GITHUB_PROJECT_NAME="${ProjectName}"
-            AKK0RD87_DEPLOY_KEY="${DeployKey}"
-            GRADLE_CALL="${WORKSPACE}/${AKK0RD87_GITHUB_PROJECT_NAME}/proj.android/gradlew -p ${WORKSPACE}/${AKK0RD87_GITHUB_PROJECT_NAME}/proj.android"
             AKK0RD_SDK_BRANCH='master'
             PROJECT_BRANCH='master'
         }
@@ -31,7 +28,7 @@ def call(String ProjectName, String DeployKey) {
             stage("Checkout project") {
                 steps {
                     checkout scmGit(branches: [[name: "${PROJECT_BRANCH}"]], extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${ProjectName}"]], userRemoteConfigs: [[credentialsId: "${DeployKey}", url: "git@github.com:akk0rd87/${ProjectName}.git"]])
-                    sh 'chmod +x ${AKK0RD87_GITHUB_PROJECT_NAME}/proj.android/gradlew'
+                    sh "chmod +x ${WORKSPACE}/${ProjectName}/proj.android/gradlew"
                 }
             }
 
