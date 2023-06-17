0f70eff1fdd298cc8483dbb2d09646331292af9b
Amir <amkad87@gmail.com>
2023-06-18 02:40:06 +0400

    Dockerfile

--- dockerfile/agent/run.sh
+++ dockerfile/agent/run.sh
@@ -1,1 +1,1 @@
-docker run -d --platform linux/amd64 --restart=on-failure -v /opt/android-sdk/:/opt/android-sdk/ -p 8022:22 localsshserver
+docker run -d --platform linux/amd64 --restart=on-failure -v /opt/android-sdk/:/opt/android-sdk/ -p 8022:22 jenkinsagent
