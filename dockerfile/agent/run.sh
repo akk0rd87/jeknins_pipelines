702bda8f1620e11cabe4a996405ccaf829555f1e
Amir <amkad87@gmail.com>
2023-06-20 10:12:19 +0400

    +

--- dockerfile/agent/run.sh
+++ dockerfile/agent/run.sh
@@ -2,4 +2,5 @@ docker run -d --platform linux/amd64 --restart=on-failure -v /opt/android-sdk/:/
 docker run -d --platform linux/amd64 --restart=on-failure -v /opt/android-sdk/:/opt/android-sdk/ -p 8022:22 alpineagent
 
 docker build . -t alpineagent
+docker build --platform linux/amd64 -t te-grafana-dashboards-toolchain --no-cache .
 docker run -it --rm --platform linux/amd64 -p 8022:22  amd64/alpine:latest
