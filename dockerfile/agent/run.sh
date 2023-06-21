8b0a12c2b28d86dbdc739633993dfe7f2098b5c8
Amir <amkad87@gmail.com>
2023-06-21 11:59:11 +0400

    docker

--- dockerfile/agent/run.sh
+++ dockerfile/agent/run.sh
@@ -1,6 +1,10 @@
 docker run -d --platform linux/amd64 --restart=on-failure -v /opt/android-sdk/:/opt/android-sdk/ -p 8022:22 jenkinsagent
 docker run -d --platform linux/amd64 --restart=on-failure -v /opt/android-sdk/:/opt/android-sdk/ -p 8022:22 alpineagent
 
+
+docker run -d --platform linux/amd64 --restart=on-failure -p 8022:22 jenkinsagent
+
 docker build . -t alpineagent
-docker build --platform linux/amd64 -t te-grafana-dashboards-toolchain --no-cache .
+docker build --platform linux/amd64 -t alpineagent --no-cache .
+docker build --platform linux/amd64 -t jenkinsagent --no-cache .
 docker run -it --rm --platform linux/amd64 -p 8022:22  amd64/alpine:latest
