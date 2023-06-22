ed5e7f1d654f7c7d1fc260f9e5d47c158cf87cdc
Amir <amkad87@gmail.com>
2023-06-22 21:20:50 +0400

    docker

--- dockerfile/agent/alpine_amd64.sh
+++ dockerfile/agent/alpine_amd64.sh
@@ -18,16 +18,15 @@ echo 'jenkins:Strongpa55word' | chpasswd && \
 chown -R jenkins:jenkins /home/jenkins && \
 chown -R jenkins:jenkins ${ANDROID_SDK_ROOT} && \
 grep -q '^#*PasswordAuthentication' /etc/ssh/sshd_config && sed -i '/^#*PasswordAuthentication[[:space:]]yes/c\PasswordAuthentication no' /etc/ssh/sshd_config || echo 'PasswordAuthentication no' >> /etc/ssh/sshd_config && \
-grep -q '^#*PermitRootLogin' /etc/ssh/sshd_config && sed -i '/^#*PermitRootLogin[[:space:]]/c\PermitRootLogin no #' /etc/ssh/sshd_config || echo 'PermitRootLogin no' >> /etc/ssh/sshd_config
-#grep -q '^#*UsePAM' /etc/ssh/sshd_config && sed -i '/^#*UsePAM[[:space:]]yes.*/c\UsePAM no' /etc/ssh/sshd_config || echo 'UsePAM no' >> /etc/ssh/sshd_config && \
-#grep -q '^#*KbdInteractiveAuthentication' /etc/ssh/sshd_config && sed -i '/^#*KbdInteractiveAuthentication[[:space:]]yes/c\KbdInteractiveAuthentication no' /etc/ssh/sshd_config || echo 'KbdInteractiveAuthentication no' >> /etc/ssh/sshd_config
+grep -q '^#*PermitRootLogin' /etc/ssh/sshd_config && sed -i '/^#*PermitRootLogin[[:space:]]/c\PermitRootLogin no #' /etc/ssh/sshd_config || echo 'PermitRootLogin no' >> /etc/ssh/sshd_config && \
+apk del wget unzip sed --quiet
 
 USER jenkins
 WORKDIR /home/jenkins
 RUN mkdir -p /home/jenkins/.ssh && mkdir -p /home/jenkins/build && \
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDDsxWrl38jhFwZFiJ8CXAR9yFERrHW8hZEMrfr8h37BHCweEpAnzq7Ue7rBLfe5FbxtmIXpm/nPwvCymZEV+BJCxgPQUpqbreJq/q4bus0REze74/i82q1wQ7AL0qLgRgqjnMfx/hx1JugKj0m6vEys9EWPHeHVI27DKojjgF2kO5pB4E94du8m54zwY/ujQ/tl9rr2euztUiQ4Cz5cOkl2In2oDC6EIO3b+NXv9yeEByHEDXKy/xUeO+JivG2sBhSKJSAvhQroKYK3RPq/AArfsheKe4zAvxkeQVjaCJ0DRSXqN9rz6x8QMOdhA/QkjSlKyg2VT4tRwGJveb1s2Sf akk0rd87@public-instance' >> /home/jenkins/.ssh/authorized_keys && \
 chmod 600 /home/jenkins/.ssh/authorized_keys &&\
-yes | sdkmanager --licenses
+yes | sdkmanager --licenses && sdkmanager "platforms;android-31" "build-tools;30.0.2"
 
 USER root
 EXPOSE 22
