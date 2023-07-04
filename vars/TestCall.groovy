80fe378dd8d10b2049097fbc5bf185d02a5db43b
Amir <amkad87@gmail.com>
2023-07-04 19:51:28 +0400

    TestCall.groovy

---
+++ vars/TestCall.groovy
@@ -0,0 +1,56 @@
+def gradleCall(String key, String params, String GRADLE_TASK) {
+    withCredentials([file(credentialsId: "${key}"   , variable: 'ANDROID_KEYSTORE_KEY'),
+                     file(credentialsId: "${params}", variable: 'ANDROID_KEYSTORE_PARAMS'),
+                     file(credentialsId: 'GooglePlayApiCredentials' , variable: 'ANDROID_GOOGLEPLAY_CREDS')]) {
+        sh '${PROJECT_DIR}/gradlew -p ${PROJECT_DIR} ' + GRADLE_TASK + ' --no-daemon'
+    }
+}
+
+def call(
+    String ProjectDir,
+    String ProjectURL,
+    String DeployKey,
+    String KeyStoreKeyFile,
+    String KeyStoreKeyParams,
+    String SDKBranch,
+    String ProjectBranch
+) {
+    stages {
+        stage('Checkout akkordsdk') {
+            steps {
+                checkout scmGit(branches: [[name: "${SDKBranch}"]], extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${AKKORD_SDK_DIR}"]], userRemoteConfigs: [[url: 'https://github.com/akk0rd87/akk0rdsdk.git']])
+            }
+        }
+
+        stage("Checkout project") {
+            steps {
+                checkout scmGit(branches: [[name: "${ProjectBranch}"]], extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${ProjectDir}"]], userRemoteConfigs: [[credentialsId: "${DeployKey}", url: "${ProjectURL}"]])
+                sh 'chmod +x ${PROJECT_DIR}/gradlew'
+            }
+        }
+
+        stage('Build debug') {
+            steps {
+                gradleCall("${KeyStoreKeyFile}", "${KeyStoreKeyParams}", '_jenkinsBuildDebug')
+            }
+        }
+
+        stage('Build release') {
+            steps {
+                gradleCall("${KeyStoreKeyFile}", "${KeyStoreKeyParams}", '_jenkinsBuildRelease')
+            }
+        }
+
+        stage('Test') {
+            steps {
+                gradleCall("${KeyStoreKeyFile}", "${KeyStoreKeyParams}", '_jenkinsTest')
+            }
+        }
+
+        stage('Publish') {
+            steps {
+                gradleCall("${KeyStoreKeyFile}", "${KeyStoreKeyParams}", '_jenkinsPublish')
+            }
+        }
+    }
+}
