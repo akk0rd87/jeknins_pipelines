438c0a9f033571e0fc253494ed6043132bc2e6e7
Amir <amkad87@gmail.com>
2023-07-11 21:44:50 +0400

    docke build and run

--- dockerfile/agent/run.sh
+++ dockerfile/agent/run.sh
@@ -1,28 +1,14 @@
-docker run -d --platform linux/amd64 --restart=on-failure -v /opt/android-sdk/:/opt/android-sdk/ -p 8022:22 jenkinsagent
-docker run -d --platform linux/amd64 --restart=on-failure -v /opt/android-sdk/:/opt/android-sdk/ -p 8022:22 alpineagent
-
-docker build -t jenkinsagent --no-cache .
-docker run -d --restart=always -p 8022:22 jenkinsagent
-
-## arm64
-docker run -d --platform linux/amd64 --restart=on-failure -p 8022:22 jenkinsagent
-
-docker build . -t alpineagent
-docker build --platform linux/amd64 -t alpineagent --no-cache .
+# build
 docker build --platform linux/amd64 -t jenkinsagent --no-cache .
-docker run -it --rm --platform linux/amd64 -p 8022:22  amd64/alpine:latest
+# run
+docker run -d --restart=always --platform linux/amd64 -p 8022:22 jenkinsagent
+
 
 ## host
 docker build -t alpinehost --no-cache .
 docker run -d --restart=on-failure -p 8822:22 alpinehost
 
-
+## connect to container
 docker exec -it --user root fervent_mclean bash
 
-
-
-docker build -t jenkinsagent --no-cache .
-
-
-
-docker run -it --rm qemu/x86-64:ovmf
+docker run --privileged --rm tonistiigi/binfmt:master --install amd64
