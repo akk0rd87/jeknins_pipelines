c88d697c9681f3c51b787d31e5f4a3b4232e843c
Amir <amkad87@gmail.com>
2023-07-22 10:37:25 +0400

    CMakeCall.groovy

---
+++ vars/CMakeCall.groovy
@@ -0,0 +1,42 @@
+def call(
+    String ProjectDir,
+    String ProjectURL,
+    String DeployKey,
+    String ProjectBranch,
+    String AgentLabel
+) {
+    pipeline {
+        agent {
+            label "${AgentLabel}"
+        }
+
+        environment {
+            PROJECT_DIR="${WORKSPACE}/${ProjectDir}/"
+        }
+
+        stages {
+            stage("Checkout project") {
+                steps {
+                    checkout scmGit(branches: [[name: "${ProjectBranch}"]], extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${ProjectDir}"]], userRemoteConfigs: [[credentialsId: "${DeployKey}", url: "${ProjectURL}"]])
+                    sh 'chmod +x ${PROJECT_DIR}/gradlew'
+                }
+            }
+
+            stage('Test') {
+                steps {
+                    dir("${PROJECT_DIR}") {
+                        sh 'cmake -S utests -B utests/build && cmake --build utests/build && ctest --test-dir utests/build'
+                    }
+                }
+            }
+        }
+
+        post {
+            always {
+                dir("${WORKSPACE}") {
+                    deleteDir()
+                }
+            }
+        }
+    }
+}
