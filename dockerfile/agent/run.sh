ff39fe6b5547a0b37ff2870ea7daabf8fcb07a6b
Amir <amkad87@gmail.com>
2024-06-09 02:26:42 +0400

    add openjdk-19-jdk-headless for Kotlin multiplatform

--- dockerfile/agent/run.sh
+++ dockerfile/agent/run.sh
@@ -1,17 +1,5 @@
-# build
-docker build --platform linux/amd64 -t jenkinsagent --no-cache .
-# run
-docker run -d --restart=always --platform linux/amd64 -p 8022:22 jenkinsagent
-
-
-## host
-docker build -t alpinehost --no-cache .
-docker run -d --restart=on-failure -p 8822:22 alpinehost
-
 ## connect to container
 docker exec -it --user root fervent_mclean bash
 
-docker run --privileged --rm tonistiigi/binfmt:master --install amd64
-
-#
+# build and run
 docker build -t jenkinsagent --no-cache . && docker run -d --restart=always -p 8022:22 jenkinsagent
